827f2295f2f50ed861ea087a97b8e7fb0abb93ef
AnthonyC247 <ac5891358@gmail.com>
2024-06-28 07:05:43 +0000

    incorporating the test file for the main file

---
+++ test_main.py
@@ -0,0 +1,29 @@
+import io
+from unittest import TestCase
+from unittest.mock import patch
+from main import main
+
+class TestFootballDataFetcher(TestCase):
+
+    @patch('builtins.input', side_effect=['6'])
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_exit_program(self, mock_stdout, mock_input):
+        main()
+        output = mock_stdout.getvalue()
+        self.assertIn("Thank you for using Football Data Fetcher!                     Exiting the program.", output)
+
+    @patch('builtins.input', side_effect=['3', 'Real Madrid', 'Premier League', '2021', 'yes', '6'])
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_real_madrid_in_prem(self, mock_stdout, mock_input):
+        with patch('football_api.fetch_team_stats') as mock_fetch:
+            mock_fetch.return_value = True
+            main()
+            output = mock_stdout.getvalue()
+            self.assertIn("Real Madrid", output)
+            self.assertIn("Premier League", output)
+            self.assertIn("England", output)
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main()
+
