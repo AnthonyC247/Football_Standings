8fc7cb316e140dd72987d3ab736739f3650a8dd8
AnthonyC247 <ac5891358@gmail.com>
2024-06-28 15:26:56 +0000

    lets see if github reports more style errors

--- test_main.py
+++ test_main.py
@@ -11,9 +11,15 @@ class TestFootballDataFetcher(unittest.TestCase):
     def test_exit_program(self, mock_stdout, mock_input):
         main()
         output = mock_stdout.getvalue()
-        self.assertIn("Thank you for using Football Data Fetcher! Exiting the program.", output)
+        self.assertIn(
+            "Thank you for using Football Data Fetcher! Exiting the program.",
+            output
+        )
 
-    @patch('builtins.input', side_effect=['3', 'Real Madrid', 'Premier League', '2021', 'yes', '6'])
+    @patch(
+        'builtins.input',
+        side_effect=['3', 'Real Madrid', 'Premier League', '2021', 'yes', '6']
+    )
     @patch('sys.stdout', new_callable=io.StringIO)
     def test_real_madrid_in_prem(self, mock_stdout, mock_input):
         with patch('football_api.fetch_team_stats') as mock_fetch:
@@ -26,5 +32,6 @@ class TestFootballDataFetcher(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
 
 
