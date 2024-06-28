8daf975b4b04cc87abdd0e9fa76392e4e5086c52
AnthonyC247 <ac5891358@gmail.com>
2024-06-28 08:00:54 +0000

    re adding the test file for main for style checking

--- test_main.py
+++ test_main.py
@@ -1,16 +1,16 @@
 import io
-from unittest import TestCase
+import unittest
 from unittest.mock import patch
 from main import main
 
-class TestFootballDataFetcher(TestCase):
+class TestFootballDataFetcher(unittest.TestCase):
 
     @patch('builtins.input', side_effect=['6'])
     @patch('sys.stdout', new_callable=io.StringIO)
     def test_exit_program(self, mock_stdout, mock_input):
         main()
         output = mock_stdout.getvalue()
-        self.assertIn("Thank you for using Football Data Fetcher!                     Exiting the program.", output)
+        self.assertIn("Thank you for using Football Data Fetcher! Exiting the program.", output)
 
     @patch('builtins.input', side_effect=['3', 'Real Madrid', 'Premier League', '2021', 'yes', '6'])
     @patch('sys.stdout', new_callable=io.StringIO)
@@ -24,6 +24,6 @@ class TestFootballDataFetcher(TestCase):
             self.assertIn("England", output)
 
 if __name__ == '__main__':
-    import unittest
     unittest.main()
+
 
